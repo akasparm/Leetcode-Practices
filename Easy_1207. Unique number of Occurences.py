d93ab7d2697ec9021555c0134cd17ba5c574bc00
akasparm <akasparm@umd.edu>
2024-07-16 16:55:34 -0400

    Easy 1207

---
+++ Easy_1207. Unique number of Occurences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        arr_dict = dict()
+        
+        for a in arr:
+            try:
+                arr_dict[a] += 1
+            except:
+                arr_dict[a] = 1
+            print(arr_dict)
+            
+        temp_list = []
+        for a in arr_dict:
+            if arr_dict[a] in temp_list:
+                return False
+            temp_list.append(arr_dict[a])
+    
+        return True
