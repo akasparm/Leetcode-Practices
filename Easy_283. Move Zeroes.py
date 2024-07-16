56397c1bb5c765df80b7d324e092668480c34408
akasparm <akasparm@umd.edu>
2024-07-15 20:39:59 -0400

    Easy 283

---
+++ Easy_283. Move Zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        i = 0
+        while i<len(nums):
+            if nums[i] == 0:
+                nums.remove(nums[i])
+                nums.append(0)
+                # print(nums)
+                # print(nums[i])
+            i+=1
