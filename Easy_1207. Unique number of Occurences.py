f8a5e0f2ecf0c646b2109a111948d17c72b7ab27
akasparm <akasparm@umd.edu>
2024-07-16 17:01:47 -0400

    commented

--- Easy_1207. Unique number of Occurences.py
+++ Easy_1207. Unique number of Occurences.py
@@ -23,6 +23,6 @@ class Solution:
 #     def uniqueOccurrences(self, arr: List[int]) -> bool:
 #         freq = {}
 #         for x in arr:
-#             freq[x] = freq.get(x, 0) + 1
+#             freq[x] = freq.get(x, 0) + 1   # If x is not found in the dictionary, it returns the default value as 0.
 
 #         return len(freq) == len(set(freq.values()))
