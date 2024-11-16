65c16c7ebda8fe00ad67fb388220507575d1827f
akasparm <akasparm@umd.edu>
2024-11-16 11:39:06 -0500

    Final_Logic_Implementation

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
-u
+
+print("After set(20, 50, 5):", manager.segments)  # [[10, 3], [20, 5], [30, 5], [40, 5], [50, 0]]
+
+manager.set(100, 130, -5)
+print("After set(20, 50, 5):", manager.segments)  # [[1
